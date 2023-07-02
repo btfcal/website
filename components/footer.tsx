80d909e49721022a7a73e1df9fc37d1a1f1886dc
Kabir Goel <kabirgoel.kg@gmail.com>
2023-07-02 16:23:25 -0700

    Constrain footer width

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,26 +3,28 @@ import Image from "next/image"
 
 export default function Footer() {
 	return (
-		<footer className="border-t py-12 px-8">
-			<div className="flex gap-4 rounded-lg items-end">
-				{socials.map(({ link, image, name }) => (
-					<a
-						key={link}
-						className="social"
-						href={link}
-						target="_blank"
-						rel="noopener"
-						title={name}
-					>
-						<Image src={image} alt={name} width={24} height={24} />
-					</a>
-				))}
+		<footer className="border-t">
+			<div className="container py-12 px-8 mx-auto">
+				<div className="flex gap-4 rounded-lg items-end">
+					{socials.map(({ link, image, name }) => (
+						<a
+							key={link}
+							className="social"
+							href={link}
+							target="_blank"
+							rel="noopener"
+							title={name}
+						>
+							<Image src={image} alt={name} width={24} height={24} />
+						</a>
+					))}
+				</div>
+				<p className="mt-4 text-slate-500 max-w-sm">
+					We are a student group acting independently of the University of
+					California. We take full responsibility for our organization and this
+					website.
+				</p>
 			</div>
-			<p className="mt-4 text-slate-500 max-w-sm">
-				We are a student group acting independently of the University of
-				California. We take full responsibility for our organization and this
-				website.
-			</p>
 		</footer>
 	)
 }
