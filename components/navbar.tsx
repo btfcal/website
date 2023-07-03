38548687b65836d656b84e8fb343d5faf7a89b42
Kabir Goel <kabirgoel.kg@gmail.com>
2023-07-02 18:49:58 -0700

    Implement responsive navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,26 +3,32 @@ import { usePathname } from "next/navigation"
 import navLinks from "@/data/nav-links.json"
 import Logo from "./logo"
 import clsx from "clsx"
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
+import { useState } from "react"
 
 export default function Navbar() {
 	const pathname = usePathname()
 
 	return (
-		<nav className="flex flex-col md:flex-row justify-between sm:items-center px-8 py-4 text-slate-700">
-			<div>
+		<nav className="flex flex-col md:flex-row justify-between items-stretch md:items-center py-4 md:px-8 text-slate-700">
+			<input type="checkbox" id="toggle" className="hidden peer" />
+			<div className="flex justify-between items-center px-8 mb-2">
 				<a href="/" className="flex items-center gap-4">
 					<Logo />
 					<span className="font-bold font-accent text-lg">
 						Build the Future
 					</span>
 				</a>
+				<label htmlFor="toggle" className="cursor-pointer md:hidden">
+					<HamburgerMenuIcon />
+				</label>
 			</div>
-			<ul className="flex gap-6">
+			<ul className="hidden peer-checked:flex md:flex flex-col md:flex-row shadow-sm border-y bg-white md:bg-transparent md:shadow-none md:border-none">
 				{navLinks.map(({ link, title }) => (
 					<a
 						key={link}
 						className={clsx(
-							"hover:text-primary underline-offset-4 underline hover:decoration-primary transition-colors",
+							"hover:text-primary underline-offset-4 underline hover:decoration-primary transition-colors py-4 px-8 md:py-0",
 							pathname === link
 								? "font-bold decoration-inherit"
 								: "decoration-transparent",
