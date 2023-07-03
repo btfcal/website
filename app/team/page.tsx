e5084f630805ba1a6dd825490c8b64d33c465e99
Kabir Goel <kabirgoel.kg@gmail.com>
2023-07-03 02:35:17 -0700

    Finally happy with the team page

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,6 +2,40 @@ import team from "@/data/team.json"
 import alumni from "@/data/alumni.json"
 import Image from "next/image"
 import Container from "@/components/container"
+import sortBy from "lodash-es/sortBy"
+
+interface SocialsProps {
+	person: Person
+}
+function Socials({ person }: SocialsProps) {
+	const socials = ["linkedin", "twitter", "email"] as const
+
+	return (
+		<div className="mt-4 flex gap-2">
+			{socials.map(
+				(social) =>
+					person[social] && (
+						<a
+							key={social}
+							href={
+								social === "email" ? `mailto:${person[social]}` : person[social]
+							}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="mr-2"
+						>
+							<Image
+								src={`/vectors/social/${social}.svg`}
+								alt={social}
+								width={16}
+								height={16}
+							/>
+						</a>
+					),
+			)}
+		</div>
+	)
+}
 
 interface Person {
 	name: string
@@ -19,60 +53,34 @@ function People({
 	people: Person[]
 	order: "asc" | "desc"
 }) {
-	const socials = ["linkedin", "twitter", "email"] as const
-	// The factor is 1 if order is ascending, -1 if descending.
-	// This basically inverts the sort comparison function if order is
-	// descending.
-	const factor = order === "asc" ? 1 : -1
-	return people
-		.sort((a, b) => {
-			// Sort by class first, then by name.
-			if (a.class < b.class) return factor * -1
-			if (a.class > b.class) return factor * 1
-			return 0
-		})
-		.map((person) => (
-			<div className="w-28 md:w-48" key={person.name}>
-				<div className="w-full relative h-28 md:h-48 mb-4">
-					<Image
-						src={require(`@/public/images/team/${person.image}`)}
-						alt={`Picture of ${person.name}`}
-						fill
-						placeholder="blur"
-						className="object-cover object-top rounded-lg shadow-sm"
-					/>
-				</div>
-				<p className="text-slate-800 font-medium">
+	const results = sortBy(people, ["class"]).map((person) => (
+		<div
+			className="flex md:flex-col items-center md:items-start gap-4"
+			key={person.name}
+		>
+			<Image
+				src={`/images/team/${person.image}`}
+				alt={`Picture of ${person.name}`}
+				width="200"
+				height="200"
+				className="object-cover object-top rounded-lg shadow-sm w-24 h-24 md:w-48 md:h-48"
+			/>
+			<div>
+				<p className="text-slate-800 font-medium text-lg">
 					{person.name} ‘{person.class}
 				</p>
 				<p className="text-slate-500 text-sm">{person.major}</p>
-				<div className="flex mt-2 gap-1">
-					{socials.map(
-						(social) =>
-							person[social] && (
-								<a
-									key={social}
-									href={
-										social === "email"
-											? `mailto:${person[social]}`
-											: person[social]
-									}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="mr-2"
-								>
-									<Image
-										src={`/vectors/social/${social}.svg`}
-										alt={social}
-										width={16}
-										height={16}
-									/>
-								</a>
-							),
-					)}
-				</div>
+				<Socials person={person} />
 			</div>
-		))
+		</div>
+	))
+	if (order === "desc") results.reverse()
+
+	return (
+		<div className="grid md:grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8 mt-12">
+			{results}
+		</div>
+	)
 }
 
 export default function Team() {
@@ -82,13 +90,9 @@ export default function Team() {
 			<p className="text-lg sm:text-xl max-w-lg text-slate-600">
 				We’re a small team of facilitators with an ambitious mission.
 			</p>
-			<div className="flex flex-wrap gap-8 mt-12">
-				<People people={team} order="asc" />
-			</div>
+			<People people={team} order="asc" />
 			<h2 className="text-3xl mt-12 font-accent font-bold">Alumni</h2>
-			<div className="flex flex-wrap gap-8 mt-12">
-				<People people={alumni} order="desc" />
-			</div>
+			<People people={alumni} order="desc" />
 		</Container>
 	)
 }
