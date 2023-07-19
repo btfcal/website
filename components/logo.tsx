f7535d8d404ee62080f9e43672347ecb5dcb84f2
Kabir Goel <kabirgoel.kg@gmail.com>
2023-07-19 13:51:05 -0700

    Make BTF logo priority

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,5 +2,13 @@ import Image from "next/image"
 import logo from "@/public/logo.png"
 
 export default function Logo() {
-	return <Image alt="Build the Future logo" width="64" height="38" src={logo} />
+	return (
+		<Image
+			priority
+			alt="Build the Future logo"
+			width="64"
+			height="38"
+			src={logo}
+		/>
+	)
 }
