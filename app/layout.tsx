278c7e7e9aa1d239dd01c1c122db2e8ea9e06045
Kabir Goel <kabirgoel.kg@gmail.com>
2023-08-03 16:45:45 -0700

    Remove unnecessary meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,6 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet="utf-8" />
-				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</head>
 			<body className="font-primary antialiased">
